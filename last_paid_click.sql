b634f8c52b0815a8bf7c8e3aef21ef7c958fdd19
Vbhfbk <inlimine2023@gmail.com>
2025-07-12 12:54:46 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,40 +1,25 @@
-with paid_click_leads as (
+select *
+from (
     select distinct on (l.visitor_id)
         l.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
-        l.status_id,
-        s.visit_date
+        l.status_id
     from sessions as s
     left join leads as l
-        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
-    where
-        l.lead_id is not null and s.medium != 'organic'
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
+            and l.lead_id is not null
+            and s.medium != 'organic'
     order by l.visitor_id asc, s.visit_date desc
-),
-
-sessions_with_paid_click_leads as (
-    select distinct on (ss.visitor_id)
-        ss.visitor_id,
-        ss.visit_date,
-        ss.source as utm_source,
-        ss.medium as utm_medium,
-        ss.campaign as utm_campaign,
-        p.lead_id,
-        p.created_at,
-        p.amount,
-        p.closing_reason,
-        p.status_id
-    from sessions as ss
-    left join paid_click_leads as p
-        on ss.visitor_id = p.visitor_id and ss.visit_date = p.visit_date
-    order by ss.visitor_id asc, p.created_at desc nulls last
-)
-
-select *
-from sessions_with_paid_click_leads
+) as t
 order by
     amount desc nulls last,
     visit_date asc,
